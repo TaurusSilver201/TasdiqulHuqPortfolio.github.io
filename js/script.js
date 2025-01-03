b462394b80793c078ce39c5a60d40aeb71e6fc19
Tasdiqul Huq <107925819+TaurusSilver201@users.noreply.github.com>
2025-01-03 18:53:06 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+// Modal functionality for certificates
+const modal = document.getElementById("cert-modal");
+const modalImg = document.getElementById("modal-img");
+const closeModal = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll('.cert-img').forEach(img => {
+  img.addEventListener('click', function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  });
+});
+
+closeModal.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // Optional: Add any other JavaScript functionality for interactivity
